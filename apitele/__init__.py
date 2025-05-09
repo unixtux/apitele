dabbdcf9c767afaf7b8456b029130dc8aa448c24
unixtux <ivangasparott@gmail.com>
2025-05-09 13:22:22 +0200

    save

--- apitele/__init__.py
+++ apitele/__init__.py
@@ -10,7 +10,7 @@ __version__ = '0.0.5'
 VERSION = __version__
 
 from .logging import get_logger
-logger = get_logger('aiotgm ' + VERSION)
+logger = get_logger('apitele ' + VERSION)
 del get_logger
 
 from .client import (
